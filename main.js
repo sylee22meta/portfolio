c63d4d033656c87c744f47ed952b998efea284b4
sylee22meta <109583366+sylee22meta@users.noreply.github.com>
2022-08-03 18:26:44 +0900

    JS: Home transparent

--- main.js
+++ main.js
@@ -16,7 +16,6 @@ document.addEventListener('scroll', () => {
 });
 
 // Handle scrolling when tapping on the navbar menu
-
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
     const target = event.target;
@@ -34,6 +33,14 @@ const homeContactBtn = document.querySelector('.home__contact');
 homeContactBtn.addEventListener('click', () => {
     scrollIntoView('#contact');
 });
+
+// Make home slowly fade to transparent as the window scrolls down
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', ()=>{
+    home.style.opacity = 1 - window.scrollY / homeHeight;
+})
+
 
 function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
