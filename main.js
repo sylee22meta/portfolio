bac6f0bbeb139f66ca4c101fb93f4ba23f55b28e
sylee22meta <109583366+sylee22meta@users.noreply.github.com>
2022-08-03 16:24:48 +0900

    JS: Navbar

---
+++ main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+
+
+// Make navbar transparent when it is on the top
+document.addEventListener('scroll', () => {
+    console.log(window.scrollY);
+    console.log(`navbarHeight: ${navbarHeight}`);
+    if (window.scrollY > navbarHeight) {
+        navbar.classList.add('navbar--dark')
+    } else {
+        navbar.classList.remove('navbar--dark')
+    }
+});
+
